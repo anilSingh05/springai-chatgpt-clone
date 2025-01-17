85db793ec28662ddd2f2dfc1e838f55c751d6178
Anil Singh <anil05.me@gmail.com>
2025-01-16 20:47:36 -0600

    Initial commit

---
+++ src/main/java/springai_chatgpt_clone/SpringaiChatgptCloneApplication.java
@@ -0,0 +1,13 @@
+package springai_chatgpt_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiChatgptCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiChatgptCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springai_chatgpt_clone/SpringaiChatgptCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package springai_chatgpt_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiChatgptCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
